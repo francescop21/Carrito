890ac5a8d4bca7fcd251fe45835b5cbb97c46ca1
Francesco Pagano <66972159+francescop21@users.noreply.github.com>
2020-10-11 02:26:43 -0300

    Proyecto Final
    Carrito de compras

---
+++ js/compra.js
@@ -0,0 +1,50 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector("#lista-compra tbody");
+const carrito = document.getElementById('carrito');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
+
+
+cargarEventos();
+
+function cargarEventos() {
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+
+    //Eliminar productos del carrito
+    carrito.addEventListener('click', (e) => { compra.eliminarProducto(e) });
+
+    compra.calcularTotal();
+
+    //cuando se selecciona procesar Compra
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+    carrito.addEventListener('change', (e) => { compra.obtenerEvento(e) });
+    carrito.addEventListener('keyup', (e) => { compra.obtenerEvento(e) });
+
+
+}
+
+function procesarCompra() {
+    // e.preventDefault();
+    if (compra.obtenerProductosLocalStorage().length === 0) {
+        Swal.fire({
+            type: 'error',
+            title: 'Error',
+            text: 'No seleccionaste ningún producto',
+            showConfirmButton: false,
+            timer: 2000
+        }).then(function () {
+            window.location = "index.html";
+        })
+    }
+    else if (cliente.value === '' || correo.value === '') {
+        Swal.fire({
+            type: 'error',
+            title: 'Faltan datos',
+            text: 'Recordá llenar todos los campos',
+            showConfirmButton: false,
+            timer: 2000
+        })
+    }
+}
+
